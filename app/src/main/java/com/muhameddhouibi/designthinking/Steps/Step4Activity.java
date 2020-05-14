29fc09f2daa52dd73adde25819b7d167cb95bc94
mohameddhouibi <mohamed.dhouibi@esprit.tn>
2020-05-14 01:43:50 +0100

    DesignThinking

---
+++ app/src/main/java/com/muhameddhouibi/designthinking/Steps/Step4Activity.java
@@ -0,0 +1,16 @@
+package com.muhameddhouibi.designthinking.Steps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.muhameddhouibi.designthinking.R;
+
+public class Step4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step4);
+    }
+}
