7312373d75f563adb483fee3c322cfe047797578
mohameddhouibi <mohamed.dhouibi@esprit.tn>
2020-05-31 01:57:10 +0100

    DesignThinking

--- app/src/main/java/com/muhameddhouibi/designthinking/WorkshopActivity.java
+++ app/src/main/java/com/muhameddhouibi/designthinking/WorkshopActivity.java
@@ -46,16 +46,19 @@ import java.util.List;
 public class WorkshopActivity extends AppCompatActivity  {
 
     RecyclerView recyclerView ;
-    Button create_game ;
+    Button create_game ,private_tn , public_btn;
     FirebaseAuth mAuth;
     private FirebaseRecyclerOptions<Room> options;
+    private FirebaseRecyclerOptions<Room> options1;
     private FirebaseRecyclerAdapter<Room,MyGameViewHolder> adapter;
     DatabaseReference publicrooms ;
     DatabaseReference privaterooms ;
+     DatabaseReference codeRef ;
+
 
     FirebaseDatabase firebaseDatabase ;
     Button confbtn , annulbtn ;
-    EditText info1 , info2 , info3 , info4 ;
+    EditText info1 , info2 , info3 , info4 ,code;
     ImageView closeDialog ;
     Dialog Infodiaog ;
     CheckBox cb1,cb2 ;
@@ -102,58 +105,168 @@ public class WorkshopActivity extends AppCompatActivity  {
             }
         });
         //end Bottom Navigation //
-
-
         firebaseDatabase = FirebaseDatabase.getInstance();
-
         mAuth = FirebaseAuth.getInstance();
-
+        private_tn=findViewById(R.id.private_Rooms);
+        public_btn=findViewById(R.id.public_Rooms);
+        recyclerView = findViewById(R.id.listgames);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         publicrooms=FirebaseDatabase.getInstance().getReference("Rooms").child("Public").child("General");
         privaterooms=FirebaseDatabase.getInstance().getReference("Rooms").child("Private").child("General");
 
-
-        recyclerView = findViewById(R.id.listgames);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        options =new FirebaseRecyclerOptions.Builder<Room>().setQuery(publicrooms,Room.class).build();
-
-        adapter=new FirebaseRecyclerAdapter<Room, MyGameViewHolder>(options) {
+        public_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected void onBindViewHolder(@NonNull MyGameViewHolder holder, int position, @NonNull Room model) {
-                final String roomid = model.getRoom_id();
-                final String nbrplayer = model.getNb_of_players();
-                final String roomname = model.getRoom_name();
-
-                holder.btn_invite.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                options =new FirebaseRecyclerOptions.Builder<Room>().setQuery(publicrooms,Room.class).build();
+                adapter=new FirebaseRecyclerAdapter<Room, MyGameViewHolder>(options) {
                     @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(WorkshopActivity.this,TestGameActivity.class);
-                        i.putExtra("roomid",""+roomid);
-                        i.putExtra("roomname",""+roomname);
-                        i.putExtra("nbr",""+nbrplayer);
-                        startActivity(i);
-                        overridePendingTransition(0,0);
+                    protected void onBindViewHolder(@NonNull MyGameViewHolder holder, int position, @NonNull Room model) {
+                        final String roomid = model.getRoom_id();
+                        final String nbrplayer = model.getNb_of_players();
+                        final String roomname = model.getRoom_name();
 
+                        holder.btn_invite.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent i = new Intent(WorkshopActivity.this,TestGameActivity.class);
+                                i.putExtra("roomid",""+roomid);
+                                i.putExtra("roomname",""+roomname);
+                                i.putExtra("nbr",""+nbrplayer);
+                                startActivity(i);
+                                overridePendingTransition(0,0);
+
+                            }
+                        });
+                        holder.GameName.setText(""+model.getRoom_name());
                     }
-                });
-                holder.GameName.setText(""+model.getRoom_name());
+
+
+                    @NonNull
+                    @Override
+                    public MyGameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_game,parent,false);
+
+                        return new MyGameViewHolder(v);
+                    }
+                };
+
+                adapter.startListening();
+
+                recyclerView.setAdapter(adapter);
             }
-
-
-            @NonNull
+        });
+        private_tn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public MyGameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            public void onClick(View v) {
+                options1 =new FirebaseRecyclerOptions.Builder<Room>().setQuery(privaterooms,Room.class).build();
+                adapter=new FirebaseRecyclerAdapter<Room, MyGameViewHolder>(options1) {
+                    @NonNull
+                    @Override
+                    public MyGameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_game,parent,false);
 
-                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_game,parent,false);
+                        return new MyGameViewHolder(v);                    }
 
-                return new MyGameViewHolder(v);
+                    @Override
+                    protected void onBindViewHolder(@NonNull MyGameViewHolder holder, int position, @NonNull Room model) {
+                        final String roomid = model.getRoom_id();
+                        final String nbrplayer = model.getNb_of_players();
+                        final String roomname = model.getRoom_name();
+
+                        holder.btn_invite.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                //////
+                                Infodiaog.setContentView(R.layout.code_layout);
+                                confbtn=(Button) Infodiaog.findViewById(R.id.conf);
+                                annulbtn=(Button) Infodiaog.findViewById(R.id.annul);
+                                code = (EditText) Infodiaog.findViewById(R.id.info98);
+
+                                closeDialog = (ImageView) Infodiaog.findViewById(R.id.close);
+
+
+                                closeDialog.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Infodiaog.dismiss();
+                                    }
+                                });
+
+                                annulbtn.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Infodiaog.dismiss();
+                                    }
+                                });
+
+                                final String cod = code.getText().toString();
+
+                                confbtn.setOnClickListener(new View.OnClickListener() {
+                                    DatabaseReference rrf = FirebaseDatabase.getInstance().getReference("Rooms").child("Private").child("General")
+                                            .child(roomname).child(String.valueOf(code));
+
+
+                                    @Override
+                                    public void onClick(View v) {
+
+                                            rrf.addValueEventListener(new ValueEventListener() {
+                                                @Override
+                                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                                    final String value2 = dataSnapshot.getValue(String.class);
+                                                    if (value2 == cod)
+                                                    {
+                                                        Intent i = new Intent(WorkshopActivity.this,TestGameActivity.class);
+                                                        i.putExtra("roomid",""+roomid);
+                                                        i.putExtra("roomname",""+roomname);
+                                                        i.putExtra("nbr",""+nbrplayer);
+                                                        startActivity(i);
+                                                        overridePendingTransition(0,0);
+
+                                                    }
+                                                    else
+                                                    {
+                                                        Toast.makeText(WorkshopActivity.this, "Invalid Code ! ", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }
+
+                                                @Override
+                                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                                }
+                                            });
+
+
+                                    }
+                                });
+                                Infodiaog.show();
+                                //////
+
+
+
+
+                            }
+                        });
+                        holder.GameName.setText(""+model.getRoom_name());
+                    }
+                };
+                adapter.startListening();
+
+                recyclerView.setAdapter(adapter);
             }
-        };
 
-        adapter.startListening();
+        });
 
-        recyclerView.setAdapter(adapter);
+
+
+
+
+
+
+
+
 
         create_game = findViewById(R.id.creategame);
 
@@ -202,18 +315,6 @@ public class WorkshopActivity extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
 
-//                if (info1.isEmpty() || password.isEmpty()) {
-//                    showMessage("Please Verify All Field");
-//                    btnLogin.setVisibility(View.VISIBLE);
-//                    loginProgress.setVisibility(View.INVISIBLE);
-//                }
-//                else
-//                {
-//                    signIn(mail,password);
-//                }
-//
-                create_game.setText("Creating your room");
-                create_game.setEnabled(false);
                 String  privacy = "";
                 final String Room_id= publicrooms.push().getKey();
                 final String Room_name = info1.getText().toString();
@@ -221,37 +322,83 @@ public class WorkshopActivity extends AppCompatActivity  {
                 final String nb = info3.getText().toString();
                 final String subject = info4.getText().toString();
                 if (cb1.isChecked()){
-                     privacy = "Public";
+                    privacy ="Public";
+                    info2.setVisibility(View.GONE);
                 }
                 else if (cb2.isChecked()){
-                    privacy = "Private";
-                }else{
-                    Toast.makeText(WorkshopActivity.this, "Privacy of the Room is required ! ", Toast.LENGTH_SHORT).show();
+                    privacy ="Private";
                 }
 
-                if (Room_id.isEmpty() || nb.isEmpty() || code.isEmpty() || subject.isEmpty()) {
 
-                    Toast.makeText(WorkshopActivity.this, "All Fields are Required ! ", Toast.LENGTH_SHORT).show();
-               }else
+                if (privacy.equals("Public")){
+
+                    if (Room_id.isEmpty() || nb.isEmpty() || subject.isEmpty()) {
+
+                        Toast.makeText(WorkshopActivity.this, "All Fields are Required ! ", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        final String Payer1_name =mAuth.getCurrentUser().getDisplayName();
+                        Room room = new Room(Room_id,Room_name,nb,Payer1_name,"public");
+                        publicrooms.child(Room_name).setValue(room);
+                        Toast toast=Toast. makeText(getApplicationContext(),"Done !",Toast. LENGTH_SHORT);
+                        toast. show();
+                        Infodiaog.dismiss();}
+
+                }
+                else if (privacy.equals("Private"))
                 {
-                    final String Payer1_name =mAuth.getCurrentUser().getDisplayName();
-                    Room room = new Room(Room_id,Room_name,nb,Payer1_name,code);
-                    if (privacy.equals("Public")){
+                    if (Room_id.isEmpty() || nb.isEmpty() || code.isEmpty() || subject.isEmpty()) {
+
+                        Toast.makeText(WorkshopActivity.this, "All Fields are Required ! ", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        final String Payer1_name =mAuth.getCurrentUser().getDisplayName();
+                        Room room = new Room(Room_id,Room_name,nb,Payer1_name,code);
                         privaterooms.child(Room_name).setValue(room);
                         Toast toast=Toast. makeText(getApplicationContext(),"Done !",Toast. LENGTH_SHORT);
                         toast. show();
-                        Infodiaog.dismiss();
-                    }
-                    else
-                    publicrooms.child(Room_name).setValue(room);
-                    Toast toast=Toast. makeText(getApplicationContext(),"Done !",Toast. LENGTH_SHORT);
-                    toast. show();
-                    Infodiaog.dismiss();
+                        Infodiaog.dismiss();}
+                }
+                else{
+                    Toast.makeText(WorkshopActivity.this, "Privacy of the Room is required ! ", Toast.LENGTH_SHORT).show();
                 }
 
+            }
+        });
+        Infodiaog.show();
+    }
+
+    private void Information2AlertBuilder ()
+    {
+        Infodiaog.setContentView(R.layout.code_layout);
+        confbtn=(Button) Infodiaog.findViewById(R.id.conf);
+        annulbtn=(Button) Infodiaog.findViewById(R.id.annul);
+        code = (EditText) Infodiaog.findViewById(R.id.info98);
+
+        closeDialog = (ImageView) Infodiaog.findViewById(R.id.close);
 
 
+        closeDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Infodiaog.dismiss();
+            }
+        });
 
+        annulbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Infodiaog.dismiss();
+            }
+        });
+
+
+        confbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                final String cod = code.getText().toString();
 
 
             }
